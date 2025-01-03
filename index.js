6a63530baefa06712ca9fb0cb820f2c714a1e859
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2025-01-03 12:13:04 +0600

    card delete done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -51,7 +51,13 @@ async function run() {
       const result = await cartCollection.find(query).toArray();
       res.send(result);
     });
-
+    // Delete a Cart
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
